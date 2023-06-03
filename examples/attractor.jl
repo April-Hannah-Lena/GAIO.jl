f40c79557512e0df0b153832a87f808f15a52f1a
April-Hannah-Lena <aprillherwig@gmail.com>
2023-06-03 12:43:39 +0200

    test changes

--- examples/attractor.jl
+++ examples/attractor.jl
@@ -8,7 +8,14 @@ center, radius = (0, 0), (3, 3)
 P = BoxPartition(Box(center, radius))
 F = BoxMap(f, P)
 S = cover(P, :)
-A = relative_attractor(F, S, steps = 16)
+A1 = relative_attractor(F, S, steps = 4)
+A2 = relative_attractor(F, S, steps = 22)
+P = A1.partition
+T = TransferOperator(F, A2, A2)
+G = BoxGraph(T)
+using Graphs, MetaGraphsNext
+using GAIO: point_to_key, key_to_box, index_to_key, key_to_index, ⊔
+ENV["JULIA_DEBUG"] = "all"
 
 using Plots: plot
 #using WGLMakie: plot    # same result, just interactive
