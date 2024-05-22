e3a134bb887cd6cc7c0ac294bf173cbb45b5e227
April Herwig <aprillherwig@gmail.com>
2024-05-22 12:02:44 +0200

    more testing

--- examples/gauss.jl
+++ examples/gauss.jl
@@ -23,6 +23,20 @@ end
 
 gauss((x,)) = (gauss(x),)
 
+jac_gauss(x::Real; α=2) = -2*α*x*exp(-α*x^2)
+jac_gauss((x,); α=2) = jac_gauss(x; α=α)
+
+doubling(x::Real) = (2x % 1) + 0.05*sinpi(4x)
+doubling((x,)) = (doubling(x),)
+
+function mob(θ::Real, α=0.8*exp(2π*im/9))
+    z = exp(2*π*im*θ)
+    z *= (z - α) / (1 - α' * z)
+    (angle(z) / π + 1) / 2
+end
+
+mob((x,)) = (mob(x),)
+
 function linear_gauss(_x::Real)
     x = _x + 1
     p = if x < 1/4
@@ -47,7 +61,9 @@ jac_linear_gauss((x,)) = jac_linear_gauss(x)
 
 ∘ⁿ(f, n) = f ∘ ( n == 1 ? identity : ∘ⁿ(f, n-1) )
 
-plot(-1:0.001:0, [identity, gauss, linear_gauss])
+plot(-1:0.001:0, [identity, gauss, linear_gauss], label=["identity" "Gauss Map" "'Crude' linear approx."])
+
+savefig("../talk_GAIO.jl/maps.png")
 
 cen, rad = (-0.5,), (0.5,)
 dom = Box(cen, rad)
@@ -81,9 +97,9 @@ function res_ulam(
     L = Diagonal([ 1/Df(c) for (c,r) in S ]) * G
 
     function residual(z, G=G, A=A, L=L)
-        ξ, _ = eigs( 
-            inv(G) * (L - z * A' - z' * A + abs(z)^2 * G),
-            which=:SM, nev=8, ritzvec=false, ncv=40, tol=0.0
+        ξ#=, _=# = eigvals( 
+            Matrix(inv(G) * (L - z * A' - z' * A + abs(z)^2 * G)),
+            #which=:SM, nev=8, ritzvec=false, ncv=40, tol=0.0
         )
         ξ0 = ξ[ argmin(abs.(ξ)) ]
         real(ξ0) < 0 && @warn "erroneous negative residual calculated" z calculated_residual=ξ0
@@ -112,7 +128,7 @@ function res_dmd(
         f = linear_gauss,
         nodes = nodes,
         weights = weights,
-        basis = leg(Val(256)),
+        basis = leg(Val(64)),#x -> [1; cospi.(2x .* (1:10)); sinpi.(2x .* (1:10))],
         xs = xs, 
         ys = ys
     )
@@ -155,20 +171,40 @@ end
 r_ulam, λ_ulam = res_ulam()
 r_dmd, λ_dmd = res_dmd()
 
-levels = [0.01; 0.02; 0.05; 0:0.1:1;]
+levels = [0.001; 0.005; 0.01; 0.02; 0.05; 0:0.1:1;]
 
 p1 = contour(
     xs, ys, r_ulam, 
     levels=levels, aspectratio=1., colormap=:rainbow, clabels=true
 )
 scatter!(λ_ulam, label="Ulam eigs", marker=:xcross)
+scatter!(λ_dmd, label="EDMD eigs", marker=:cross)
 plot!(cospi.(0:0.01:2), sinpi.(0:0.01:2), style=:dash, label="|z| = 1")
 
 p2 = contour(
     xs, ys, r_dmd, 
     levels=levels, aspectratio=1., colormap=:rainbow, clabels=true
 )
+scatter!(λ_ulam, label="Ulam eigs", marker=:xcross)
 scatter!(λ_dmd, label="EDMD eigs", marker=:cross)
 plot!(cospi.(0:0.01:2), sinpi.(0:0.01:2), style=:dash, label="|z| = 1")
+#scatter!(ComplexF64[((-0.8*exp(2π*im/9)) .^ (1:10)); ((-0.8*exp(-2π*im/9)) .^ (1:10))], marker=:xcross)
 
 plot(p1, p2, size=(1200,600))
+
+savefig("../talk_GAIO.jl/pseudospectrum_comparison_gauss.png")
+
+
+anim = @animate for n_basis in [32, 64, 128, 256]
+    n_points = 4*n_basis
+    nodes, weights = gausslegendre(n_points)
+    r_dmd, λ_dmd = res_dmd(mob, nodes, weights, leg(Val(n_basis)))
+    p3 = contour(
+        xs, ys, r_dmd, 
+        levels=levels, aspectratio=1., colormap=:rainbow, clabels=true,
+        title="$n_basis legendre polynomial basis"
+    )
+    scatter!(p3, λ_dmd, label="EDMD eigs", marker=:cross)
+    plot!(p3, cospi.(0:0.01:2), sinpi.(0:0.01:2), style=:dash, label="|z| = 1")
+end
+gif(anim, fps=0.5)
